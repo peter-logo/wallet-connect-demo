f72c3f88b3d698931ee65674286941efd539184c
Peter <peterlogo@krownpay.com>
2025-03-28 18:05:15 -0400

    chore: setup utils folders

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,11 @@
-import { HomePage } from "@/pages";
+import { HomePage, SuccessPage } from "@/pages";
 import { Route, Routes } from "react-router";
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/success" element={<SuccessPage />} />
     </Routes>
   );
 };
